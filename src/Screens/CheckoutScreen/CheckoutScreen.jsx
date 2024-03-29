b8d744196ba9f286041b015077b5d55c0f890e25
Gemba Infotech <Alok.jain@gembainfotech.com>
2024-03-29 14:04:34 +0530

    m

--- src/Screens/CheckoutScreen/CheckoutScreen.jsx
+++ src/Screens/CheckoutScreen/CheckoutScreen.jsx
@@ -67,57 +67,75 @@ function Booking() {
       },
     });
     const order = await response.json();
-    var options = {
-      "key": "rzp_test_muLBb6gKqfrZA5", // Enter the Key ID generated from the Dashboard
-      amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-      currency,
-      "name": "Gemba Infotech", //your business name
-      "description": "Test Transaction",
-      "order_id": order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-      "handler": async function (response) {
-        const body = {
-          ...response
-        };
-        const validateRes = await fetch("http://localhost:7001/v1/api/razorpay/order/validate", {
-          method: "POST",
-          body: JSON.stringify(body),
-          headers: {
-            "Content-Type": "application/json"
-          },
-        });
-
-        const jsonRes = await validateRes.json();
-        
-        if (jsonRes.msg === "success") {
-          book()
+    console.log(order)
+    const success = order.success;
+    console.log(order);
+    if(success)
+    {
+      var options = {
+        "key": "rzp_test_muLBb6gKqfrZA5", // Enter the Key ID generated from the Dashboard
+        amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        currency,
+        "name": "Gemba Infotech", //your business name
+        "description": "Test Transaction",
+        "order_id": order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+        "handler": async function (response) {
+          const body = {
+            ...response
+          };
+          const validateRes = await fetch("http://localhost:7001/v1/api/razorpay/order/validate", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+              "Content-Type": "application/json"
+            },
+          });
+  
+          const jsonRes = await validateRes.json();
+           console.log(jsonRes)
+          if (jsonRes.msg === "success") {
+            book()
+          }
+        },
+        "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+          "name": "Surabhi Yadav", //your customer's name
+          "email": "Surabhiya2001@gmail.com",
+          "contact": "8303672402"  //Provide the customer's phone number for better conversion rates 
+        },
+        "notes": {
+          "address": "Razorpay Corporate Office"
+        },
+        "theme": {
+          "color": "#3399cc"
         }
-      },
-      "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
-        "name": "Surabhi Yadav", //your customer's name
-        "email": "Surabhiya2001@gmail.com",
-        "contact": "8303672402"  //Provide the customer's phone number for better conversion rates 
-      },
-      "notes": {
-        "address": "Razorpay Corporate Office"
-      },
-      "theme": {
-        "color": "#3399cc"
+      };
+      var rzp1 = new window.Razorpay(options)
+      rzp1.on('payment.failed', function (response) {
+        alert(response.error.code);
+        alert(response.error.description);
+        alert(response.error.source);
+        alert(response.error.step);
+        alert(response.error.reason);
+        alert(response.error.metadata.order_id);
+        alert(response.error.metadata.payment_id);
+      });
+      document.getElementById('pay').onclick = function (e) {
+        rzp1.open();
+        e.preventDefault();
       }
-    };
-    var rzp1 = new window.Razorpay(options)
-    rzp1.on('payment.failed', function (response) {
-      alert(response.error.code);
-      alert(response.error.description);
-      alert(response.error.source);
-      alert(response.error.step);
-      alert(response.error.reason);
-      alert(response.error.metadata.order_id);
-      alert(response.error.metadata.payment_id);
-    });
-    document.getElementById('pay').onclick = function (e) {
-      rzp1.open();
-      e.preventDefault();
     }
+    else{
+      Swal.fire({
+        icon: 'error',
+        title: 'error',
+        text: 'Your parking does not booked successfully',
+      }).then(() => {
+        
+   console.log("flaied")          
+      });
+
+    }
+   
   };
 
   const book = async () => {
